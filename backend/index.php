38440c78a47946e735cac0fa451681159a4810ab
Ismérie George <ismeriegeorge@mac.home>
2025-10-07 13:03:42 +0200

    feat: router backend

--- backend/index.php
+++ backend/index.php
@@ -1,4 +1,8 @@
 <?php
 require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/core/Router.php';
+
+$router = new Router();
+
 
 echo "Camagru backend is running!";
---
+++ backend/routes/auth.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/../controllers/AuthController.php';
+
+function registerAuthRoutes($router) {
+    $router->add('POST', '/register', function() {
+        $data = json_decode(file_get_contents("php://input"), true);
+        AuthController::register($data);
+    });
+
+    $router->add('POST', '/login', function() {
+        $data = json_decode(file_get_contents("php://input"), true);
+        AuthController::login($data);
+    });
+
+    $router->add('POST', '/logout', function() {
+        $data = json_decode(file_get_contents("php://input"), true);
+        AuthController::logout($data);
+    });
+}
