da553cc90e6731e766a2ebfb1567967747f095d7
Ismerie George <igeorge@z4r3p3.42lyon.fr>
2026-07-07 16:04:23 +0200

    refactor: views

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -3,16 +3,16 @@
 class Controller {
     protected function render($view, $data = [], $scripts = []) {
         extract($data);
-        require_once APP . '/views/header.php';
+        require_once APP . '/views/layout/header.php';
 
         $viewsWithBackground = ['home', 'signup', 'login'];
         if (in_array($view, $viewsWithBackground)) {
-            require_once APP . '/views/backgroundSquare.php';
+            require_once APP . '/views/partials/background-square.php';
         }
 
-        require_once APP . '/views/toastContainer.php';
-        require_once APP . '/views/' . $view . '.php';
-        require_once APP . '/views/footer.php';
+        require_once APP . '/views/partials/toast-container.php';
+        require_once APP . '/views/pages/' . $view . '.php';
+        require_once APP . '/views/layout/footer.php';
 
         foreach ($scripts as $script) {
             echo '<script type="module" src="js/' . htmlspecialchars($script) . '" defer></script>';
